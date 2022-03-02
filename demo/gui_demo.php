a81096ae1a87d40ba88105a96ca1c0f2e35223d4
Pierre <dev@nettools.ovh>
2022-03-02 16:23:00 +0100

    php8

--- demo/gui_demo.php
+++ demo/gui_demo.php
@@ -73,7 +73,7 @@ catch(Throwable $e)
 ?><html>
     <body>
         <?php
-        if( $ret )
+        if( isset($ret) )
             echo "<pre style=\"background-color:black; color:white;\">$ret</pre>";
         ?>
         
